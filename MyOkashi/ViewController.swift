f09438e3a4dee6c71aa36a586f6a7b020a5e2d00
kakeru_nakabachi <baaaakkrad@gamail.com>
2018-08-16 21:41:49 +0900

    [add]SearchBar実装

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Search BarのDelegate通知先を設定
+        searchText.delegate = self
+        
+        // 入力のヒントになる，プレースホルダーを設定
+        searchText.placeholder = "お菓子の名前を入力してください"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var searchText: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // 検索ボタンタップ時
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // キーボード閉じる
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text {
+            // デバッグエリアに出力
+            print(searchWord)
+        }
+    }
 }
 
